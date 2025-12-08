55f51a662e31046db927f79234a9361422b9a2eb
ZhangZhuo-666 <710126039a@gmail.com>
2025-12-09 06:59:51 +0800

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,10 @@
 from app import app
 
-
 def test_home():
     client = app.test_client()
     res = client.get("/")
     assert res.status_code == 200
     assert res.json["message"] == "HealthyLife Tracker is running"
-
 
 def test_add_and_get_records():
     client = app.test_client()
@@ -19,11 +17,9 @@ def test_add_and_get_records():
         "sleep_hours": 7
     }
 
-    # Add
     res_add = client.post("/add", json=sample)
     assert res_add.status_code == 200
 
-    # Get
     res_get = client.get("/records")
     assert res_get.status_code == 200
     assert isinstance(res_get.json, list)
