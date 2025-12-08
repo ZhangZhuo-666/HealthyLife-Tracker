9c9f22e7383c0ef1d184215f3a81d460e7eff2bf
ZhangZhuo-666 <710126039a@gmail.com>
2025-12-09 07:00:05 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import sys
 import os
 
-# 把 src 目录加入 import 路径
+# 项目根目录
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# src 文件夹路径
 SRC_DIR = os.path.join(ROOT_DIR, "src")
 
+# 把项目根目录和 src 目录都加进 import 路径中
+sys.path.insert(0, ROOT_DIR)
 sys.path.insert(0, SRC_DIR)
